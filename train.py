c49956d95bcb721fe6cb8654ef7c1cc27ba65d2b
antrikshmisri <antrikshmisri@gmail.com>
2021-09-25 11:08:00 +0530

    fix: fixed the input data processing

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import json
-import input
+from itertools import count
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import UbuntuCorpusTrainer
+
 
 def ToString(s):
     # initialize an empty string
@@ -9,52 +10,98 @@ def ToString(s):
     # return string
     return (str1.join(s))
 
-with open('intents.json','r') as f:
-    intents = json.load(f)
 
+def inputdata(file='intents.json'):
+    """Process the input data to a list of statements.
 
+    Parameters
+    ----------
+    file : str
+        The path to the input file.
 
-statements = []
-ignore_words = ['.', ',', '?', '!']
-def inputdata():
-    answerindex = 0
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            if(answerindex >= len(intent['responses'])-1):
-                answerindex=0
-            else:
-                answerindex+=1
-            q = input.tokenize(pattern.lower())
-            a = input.tokenize(intent['responses'][answerindex].lower())
-            for word in q:
-                if word in ignore_words:
-                    q.remove(word)
-            for word in a:
-                if word in ignore_words:
-                    a.remove(word)
-            statements.append(ToString(q))
-            statements.append(ToString(a))
-    return statements
+    Returns
+    -------
+    list
+        A list of statements representing a conversation.
+    """
+    with open(file, 'r') as f:
+        dataset = json.load(f)
 
-#hyperparameters
+    if file == 'english-train.json':
+        statements = []
+        for conversation in dataset:
+            question = conversation['utterances'][0].lower().split('patient:')[1]
+            fullstop_questions = question.split('.')
+
+            answer = conversation['utterances'][1].lower().split('doctor:')[1]
+
+            for f_question in fullstop_questions:
+                statements.append([f_question, answer])
+
+            statements.append([question, answer])
+
+        return statements
+    
+    elif file == 'train_light.json':
+        statements = []
+        for obj in dataset:
+            annotation_dict = obj['annotations'][0]
+            question_type = annotation_dict['type']
+            if question_type == 'multipleQAs':
+                question_objs = annotation_dict['qaPairs']
+                for obj in question_objs:
+                    question = obj['question'].lower()
+                    answer = obj['answer'][0].lower()
+                    statements.append([question, answer])
+            elif question_type == 'singleAnswer':
+                question = obj['question'].lower()
+                annotation_dict = obj['annotations'][0]
+                answer = annotation_dict['answer'][0].lower()
+                statements.append([question, answer])
+    
+    elif file == 'intents.json':
+        statements = []
+        obj = dataset['intents']
+        
+        for intent in obj:
+            questions = intent['patterns']
+            answer = intent['responses']
+
+            for question, answer in zip(questions, answer):
+                statements.append([question.lower(), answer.lower()])
+
+        return statements
+
+# hyperparameters
 iter = 10
-def trainbotbylist(chatbot,iter):
-    if(iter == 0):
-        return "Sucessfully Trained Chatbot"
-    iter -= 1
+
+
+def trainbotbylist(chatbot, dataset='english-train.json'):
+    """Train the chatbot using ListTrainer on a specific dataset.
+    
+    Parameters
+    ----------
+    chatbot : ChatBot
+        A chatbot instance.
+    dataset : str
+        The path to the input file.
+    """
     trainer = ListTrainer(chatbot)
-    trainer.train(inputdata())
-    print("training the chatbot , iteration No:-" + str(iter))
-    trainbotbylist(chatbot , iter)
-def trainbotbyubuntu(chatbot,iter):
+    data = inputdata(dataset)
+    iterator = (count(start=1, step=1))
+
+    for conversation in data:
+        trainer.train(conversation)
+        statement_count = next(iterator)
+    
+    print(f'Successfully trained bot with {statement_count} conversations')
+
+
+def trainbotbyubuntu(chatbot, iter):
     if(iter == 0):
         return "Sucessfully Trained Chatbot"
     iter -= 1
     trainer = UbuntuCorpusTrainer(chatbot)
     trainer.train()
     print("training the chatbot , iteration No:-" + str(iter))
-    trainbotbylist(chatbot , iter)
-
-
-
-
+    trainbotbyubuntu(chatbot, iter)
